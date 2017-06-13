475fa7b85d8b92ae7f5d9d3cb5a13cccc2d681db
boweiling <bwl312@163.com>
2017-06-13 15:39:12 +0800

    路由

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import ReactDom from 'react-dom';
+import {
+    HashRouter as Router,//容器
+    Route//一条路由
+} from 'react-router-dom';
+function Root(){
+    return <h1>Root</h1>
+}
+function Home() {
+    return <h1>Home</h1>
+}
+function User() {
+    return <h1>User</h1>
+}
+function Profile() {
+    return <h1>Profile</h1>
+}
+ReactDom.render(
+    <Router>
+      <div>
+          <Route path="/" component={Root}/>
+          <Route path="/home" component={Home}/>
+          <Route path="/user" component={User}/>
+          <Route path="/profile" component={Profile}/>
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+      </div>
+
+    </Router>,document.querySelector('#root')
+)
